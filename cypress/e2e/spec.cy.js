b751f42fedc055d3fa4254d44a697c1fe9bda5a7
LibzAna8 <dror.kedem8@gmail.com>
2026-06-12 01:15:25 +0300

    add new e2e tests

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,7 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://practice-playful-pages.lovable.app/')
+    cy.get('[data-testid="nav-cart"]').click()
+    cy.get('[data-testid="cart-title"]').should('contain', 'Shopping Cart')
+  })
+})
