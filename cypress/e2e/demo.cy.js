a28f9cbae41c5e85b0377e0b34d4f5e281796130
LibzAna8 <dror.kedem8@gmail.com>
2026-06-12 16:51:15 +0300

    update

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,5 +1,5 @@
 it('demo tests', () => {
   cy.visit('https://practice-playful-pages.lovable.app/')
   cy.get('[data-testid="nav-cart"]').click()
-  cy.get('[data-testid="cart-title"]').should('contain', 'Shopping Cart1')
+  cy.get('[data-testid="cart-title"]').should('contain', 'Shopping Cart')
 })
