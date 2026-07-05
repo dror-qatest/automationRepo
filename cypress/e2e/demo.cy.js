12cab85132cc3720acb422ccee7feb3cef1a91c8
LibzAna8 <dror.kedem8@gmail.com>
2026-07-05 21:00:59 +0300

    commit

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,10 +1,4 @@
 it('UI Elements', () => {
   cy.visit('https://drork-automation-practice.lovable.app/practice')
-  cy.get('[data-cy="sidebar-dialogs-fra5mes"]').click()
- cy.get('[data-cy="sandbox-frame"]')                    // the <iframe> element itself
-  .its('0.contentDocument.body')                   // reach into its inner document                        // retry until iframe has loaded
-  .then(cy.wrap)                                   // wrap it so cy commands work on it
-  .find('#inner-text')
-  .should('be.visible')
-  .and('have.text', 'I live in an iframe.');
+  cy.get('[data-testid="nav-hom5555e"]').click()
 })
